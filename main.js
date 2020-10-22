5be6274561e0378516196462247385585465ff81
Reddy <mallepally.saiprajwal@optum.com>
2020-10-22 14:31:41 +0530

    Code Clean Up

--- main.js
+++ main.js
@@ -37,3 +37,27 @@ function onScroll(event){
         }
     });	
 }
+
+$(document).ready(function(){
+        
+        $(".collapse.show").each(function(){
+        	$(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
+        });
+        
+        
+        $(".collapse").on('show.bs.collapse', function(){
+        	$(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
+        }).on('hide.bs.collapse', function(){
+        	$(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
+        });
+    });
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+  x.toggle();
+}
